a877570e29e216bda0c415a39b541ebff993d7ce
nairudeen nurudeen <naseernoor4864@gmail.com>
2023-02-20 14:19:05 +0100

    first commit

--- src/component/slick-slider/slick-slider.jsx
+++ src/component/slick-slider/slick-slider.jsx
@@ -26,7 +26,7 @@ export default function SimpleSlider() {
         {SliderData.map((data) => {
           return (
             <div key={data.id} className="slider-container">
-              <img src={data.imageLink} alt="product-pics" className="slider-img" />
+              <img src={data.imageLink} loading="lazy" alt="product-pics" className="slider-img" />
               <span onClick={handleSlider}>SHOP NOW</span>
             </div>
           )
