6963fb591c476d107e74787ecf42e0d73dce4cbd
naseer4864 <naseernoor4864@gmail.com>
2023-04-29 08:57:07 +0100

    first commit

--- src/component/account/account.jsx
+++ src/component/account/account.jsx
@@ -1,17 +1,27 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faUser} from "@fortawesome/free-solid-svg-icons"
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 import { useContext } from "react";
 import { CartContext } from "../../context/cart.context";
-
+import { UserContext } from "../../context/firebase.context";
 
 const Account = () => {
-    const {isUserOpen, setIsUserOpen} = useContext(CartContext);
-    const togleIsuerOpen = () => setIsUserOpen(!isUserOpen)
-    return (
-        <div className="account-container" onClick={togleIsuerOpen}>
-           sign In <FontAwesomeIcon icon={faUser} />
-        </div>
-    );
-}
+  const { isUserOpen, setIsUserOpen } = useContext(CartContext);
+  const { currentUser } = useContext(UserContext);
+  const toggleIsUserOpen = () => setIsUserOpen(!isUserOpen);
+
+  return (
+    <div className="account-container" onClick={toggleIsUserOpen}>
+      {currentUser ? (
+        
+          <span>Account</span>
+        
+      ) : (
+        <span>
+            Sign In <FontAwesomeIcon icon={faUser} />
+        </span>
+      )}
+    </div>
+  );
+};
 
 export default Account;
